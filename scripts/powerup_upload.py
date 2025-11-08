e87317cbef9a2a0a7db54e79040a22ef460d89f9
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-11-08 15:25:41 +0900

    Update powerup_upload.py

--- scripts/powerup_upload.py
+++ scripts/powerup_upload.py
@@ -13,7 +13,7 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 # ダウンロード対象 URL と保存先　
 FILE_URL = "https://raw.githubusercontent.com/Konnitiwa768/PVPVe/c81d8dcff7e1696f525ebb78f117adb0eb174c64/RP/textures/models/armor/marisa_layer_2.png" #"https://github.com/GotembaYuka/Gote-Supporter/releases/download/v1.7.3/Gote-Supporter_v1.7.3.mcpack"#"https://github.com/Konnitiwa768/Tekscha/raw/refs/heads/main/releases/Bedwars_Mega.mcpack"#https://www.mediafire.com/file/jxwewlmnam7fm0b/Ymzie_Black.mcpack/file"# 
-FILE_NAME = "j.zip"
+FILE_NAME = "marisa_1.png"
 FILE_PATH = os.path.join(DOWNLOAD_DIR, FILE_NAME)
 
 
@@ -76,7 +76,7 @@ def main():
 
         # === STEP 1: ログインページ ===
         page.goto(
-            "https://www.powerupstack.com/auth/login?redirect=/panel/instances/komugi5/files?path=resource_packs/RP/textures/models/armor"
+            "https://www.powerupstack.com/auth/login?redirect=/panel/instances/komugi5/files?path=resource_packs%2FRP%2Ftextures%2Fmodels%2Farmor
         )
         page.wait_for_load_state("networkidle")
         page.screenshot(path=f"{SCREENSHOT_DIR}/01_login_page.png")
